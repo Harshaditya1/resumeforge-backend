e14bfdc4c38327b103139cffe4ef5a722e915b28
Harshaditya1 <patelboy90287484@gmail.com>
2026-07-23 21:30:52 +0530

    chore: initialize Spring Boot backend with PostgreSQL configuration

---
+++ src/main/java/com/resumeforge/ResumeforgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resumeforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeforgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeforgeBackendApplication.class, args);
+	}
+
+}
